0a71f77ee4beede6bffcdd9eeacc5612d35f0eff
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-09-17 13:06:53 +0530

    add official colors for cobalt2

---
+++ lua/cobalt2.lua
@@ -0,0 +1,13 @@
+require('colorbuddy').setup()
+
+local Color = require('colorbuddy').Color
+
+Color.new('yellow', '#ffc600')
+Color.new('orange', '#ff9d00')
+Color.new('mint', '#2affdf')
+Color.new('blue', '#193549')
+Color.new('off_blue', '#0d3a58 ')
+Color.new('dusty_blue', '#35434d')
+Color.new('dark_blue', '#15232d')
+Color.new('pink', '#fb94ff')
+Color.new('light_blue', '#9effff')
