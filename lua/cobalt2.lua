18c657f08b34219c87dda9c9534f2feea67003bb
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-02-24 06:51:06 +0530

    add BufferCurrent background color

--- lua/cobalt2.lua
+++ lua/cobalt2.lua
@@ -10,6 +10,63 @@ local Color, colors, Group, groups, styles = require('colorbuddy').setup()
 -- Color.new('pink', '#fb94ff')
 -- Color.new('light_blue', '#9effff')
 
+-- NOTE: default
+require('lualine').setup {
+  options = {
+    -- theme = 'auto',
+    theme = 'cobalt2',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = { { 'mode', fmt = string.upper } },
+    lualine_b = { { 'branch', icon = '' } },
+    lualine_c = {
+      { 'filetype', icon_only = true },
+      { 'filename', path = 1 },
+      {
+        'diagnostics',
+        sources = { 'coc' },
+        -- sources = { 'nvim_lsp' },
+        symbols = {
+          error = ' :',
+          warn = ' :',
+          info = ' :',
+          hint = '💡',
+        },
+        diagnostics_color = {
+          error = { fg = '#E06C75' },
+          warn = { fg = '#FF922B' },
+          info = { fg = '#15AABF' },
+          hint = { fg = '#fab005' },
+        },
+        update_in_insert = true,
+      },
+      { 'b:coc_current_function', 'g:coc_status' },
+      { 'diff' },
+    },
+    lualine_x = { { 'filesize' } },
+    lualine_y = { { 'progress' } },
+    lualine_z = { { 'location' } },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { { 'filename', path = 0 } },
+    lualine_x = { 'filetype' },
+    lualine_z = { 'location' },
+  },
+  -- tabline = {
+  --   lualine_a = { 'buffers' },
+  --   lualine_b = {},
+  --   lualine_c = {},
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = { 'tabs' },
+  -- },
+  extensions = { 'fzf', 'fugitive', 'nvim-tree', 'quickfix', 'toggleterm' },
+}
+
 -- " General colors
 Color.new('cobalt_bg', '#193549')
 Color.new('black', '#1C1C1C')
@@ -111,4 +168,5 @@ Group.new('SpellLocal', nil, colors.darkest_green, styles.underline)
 Group.new('DiffDelete', colors.black, colors.darker_red, nil)
 Group.new('DiffChange', nil, colors.darkest_green, nil)
 Group.new('DiffText', colors.greyish_blue, colors.black, nil)
-Group.new('IndentBlankLineChar', colors.lightest_grey, nil, nil)
+Group.new('IndentBlankLineChar', colors.grey, nil, nil)
+Group.new('BufferCurrent', colors.grey, colors.cobalt_bg, nil)
