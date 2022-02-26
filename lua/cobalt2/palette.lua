08c3426e99e11f03a081d99b7eeff62d88797f8d
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-02-26 22:59:38 +0530

    revert colors creating in palette

--- lua/cobalt2/palette.lua
+++ lua/cobalt2/palette.lua
@@ -1,8 +1,6 @@
 -- Copyright (c) 2022-2022 Lalit Kumar
 -- License: MIT
 --
-local Color = require('colorbuddy.color').Color
-
 local colors = {
   cobalt_bg = '#193549',
 
@@ -63,14 +61,5 @@ local colors = {
   cursor_line = '#0d3a58',
   cursor_hover = '#185294',
 }
-
--- create colors for cobalt2 theme
-local function create_colors(p)
-  for i, color in pairs(p) do
-    Color.new(i, color)
-  end
-end
-
-create_colors(colors)
 
 return colors
