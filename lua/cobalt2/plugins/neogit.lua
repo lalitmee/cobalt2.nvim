084c6f65738663007039ff2d2e3ac34f2d8da59f
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-02-27 22:22:22 +0530

    update neogit colors

--- lua/cobalt2/plugins/neogit.lua
+++ lua/cobalt2/plugins/neogit.lua
@@ -4,11 +4,11 @@ local Group = require("cobalt2.utils").Group
 ---------------------------------------------------------------------------------
 --                                   neogit                                    --
 ---------------------------------------------------------------------------------
-Group.new("NeogitBranch", colors.dark_orange, nil, nil)
+Group.new("NeogitBranch", colors.green, nil, nil)
 Group.new("NeogitRemote", colors.dark_pink, nil, nil)
 Group.new("NeogitHunkHeader", colors.light_blue, nil, nil)
 Group.new("NeogitHunkHeaderHighlight", colors.blue, nil, nil)
-Group.new("NeogitDiffContextHighlight", colors.white, colors.darker_blue:dark(), nil)
+Group.new("NeogitDiffContextHighlight", colors.white, colors.cobalt_bg:light(), nil)
 Group.new("NeogitDiffDeleteHighlight", colors.red, colors.cobalt_bg, nil)
 Group.new("NeogitDiffAddHighlight", colors.green, colors.cobalt_bg, nil)
 Group.new("NeogitDiffDelete", colors.white, colors.red, nil)
