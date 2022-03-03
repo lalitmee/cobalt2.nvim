2a7411e277ca12ff5de5206b2e8b25c5a8b7087a
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-03 18:13:53 +0530

    update some colors

--- lua/cobalt2/plugins/startify.lua
+++ lua/cobalt2/plugins/startify.lua
@@ -7,6 +7,6 @@ local Group = require("cobalt2.utils").Group
 Group.new("StartifyBracket", colors.blue, nil, nil)
 Group.new("StartifyNumber", colors.dark_pink, nil, nil)
 Group.new("Startifyfile", colors.light_green, nil, nil)
-Group.new("Startifyheader", colors.dark_pink, nil, nil)
+Group.new("Startifyheader", colors.blue, nil, nil)
 Group.new("Startifypath", colors.yellow, nil, nil)
 Group.new("Startifyslash", colors.blue, nil, nil)
--- lua/cobalt2/theme.lua
+++ lua/cobalt2/theme.lua
@@ -5,6 +5,7 @@ local Group = require("cobalt2.utils").Group
 ---------------------------------------------------------------------------------
 --                                    theme                                    --
 ---------------------------------------------------------------------------------
+-- general
 Group.new("Normal", colors.white, colors.cobalt_bg, nil)
 Group.new("Comment", colors.dark_blue, nil, styles.italic)
 Group.new("Todo", colors.dark_blue, nil, styles.bold)
@@ -28,36 +29,48 @@ Group.new("Directory", colors.yellow, nil, nil)
 Group.new("ErrorMsg", nil, colors.red, nil)
 Group.new("Question", colors.green, nil, nil)
 Group.new("StorageClass", colors.light_orange, nil, nil)
-Group.new("TabLine", colors.black, colors.lighter_grey, styles.italic)
-Group.new("TabLineFill", colors.light_grey, nil, nil)
-Group.new("TabLineSel", colors.black, colors.yellow, styles.bold)
+Group.new("Visual", nil, colors.darker_blue, nil)
+Group.new("VertSplit", colors.grey, nil, nil)
+Group.new("WildMenu", colors.pale_pink, colors.darkest_grey, nil)
+Group.new("Folded", colors.light_grey, colors.cursor_line, styles.italic)
+Group.new("Title", colors.dirty_green, nil, styles.bold)
+
+-- pmenu
 Group.new("PMenu", colors.lightest_grey, colors.cobalt_bg:dark(), nil)
 Group.new("PMenuSel", colors.darkest_grey, colors.yellow, nil)
 Group.new("PMenuSBar", nil, colors.dark_grey, nil)
 Group.new("PMenuThumb", nil, colors.lightest_grey, nil)
-Group.new("Visual", nil, colors.darker_blue, nil)
-Group.new("Cursor", colors.yellow, colors.cobalt_bg, nil)
-Group.new("CursorColumn", nil, colors.cursor_hover, nil)
-Group.new("CursorLine", nil, colors.cursor_line, nil)
-Group.new("CursorLineNr", colors.yellow, nil, styles.bold)
-Group.new("LineNr", colors.light_grey, colors.cobalt_bg, styles.NONE)
-Group.new("StatusLine", colors.black, colors.lightest_grey, styles.italic)
-Group.new("StatusLineNC", colors.white, colors.darker_grey, styles.italic)
-Group.new("VertSplit", colors.grey, nil, nil)
-Group.new("WildMenu", colors.pale_pink, colors.darkest_grey, nil)
-Group.new("Folded", colors.light_grey, colors.cursor_line, styles.italic)
+
+-- column, cursor and lines
 Group.new("FoldColumn", colors.dark_grey, colors.black, nil)
 Group.new("SignColumn", colors.cobalt_bg, nil, nil)
 Group.new("ColorColumn", colors.yellow, colors.cobalt_bg, nil)
-Group.new("Title", colors.dirty_green, nil, styles.bold)
+Group.new("CursorColumn", nil, colors.cursor_hover, nil)
+Group.new("Cursor", colors.yellow, colors.cobalt_bg, nil)
+Group.new("CursorLine", nil, colors.cursor_line, nil)
+Group.new("CursorLineNr", colors.yellow, nil, styles.bold)
+Group.new("LineNr", colors.light_grey, colors.cobalt_bg, styles.NONE)
+Group.new("StatusLine", colors.yellow, colors.cursor_line, styles.italic)
+Group.new("StatusLineNC", colors.white, colors.cursor_line, styles.italic)
+Group.new("TabLine", colors.black, colors.lighter_grey, styles.italic)
+Group.new("TabLineFill", colors.light_grey, nil, nil)
+Group.new("TabLineSel", colors.black, colors.yellow, styles.bold)
+
+-- search
 Group.new("Search", colors.black, colors.blue, styles.NONE)
 Group.new("IncSearch", colors.black, colors.yellow, styles.NONE)
+
+-- spell
 Group.new("SpellBad", colors.dark_red, nil, styles.underline)
 Group.new("SpellCap", nil, colors.darkest_blue, styles.underline)
 Group.new("SpellRare", nil, colors.aubergine, styles.underline)
 Group.new("SpellLocal", nil, colors.darkest_green, styles.underline)
+
+-- diff
 Group.new("DiffDelete", colors.black, colors.darker_red, nil)
 Group.new("DiffChange", nil, colors.darkest_green, nil)
 Group.new("DiffText", colors.greyish_blue, colors.black, nil)
+
+-- float window
 Group.new("FloatBorder", colors.blue, nil, nil)
 Group.new("NormalFloat", colors.white, colors.cobalt_bg:light(), nil)
