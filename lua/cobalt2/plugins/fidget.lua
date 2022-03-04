e9f34c5045d43ed72a9cb90980f92f14ac030543
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-04 11:46:00 +0530

    add fidget.nvim colors

---
+++ lua/cobalt2/plugins/fidget.lua
@@ -0,0 +1,9 @@
+local colors = require("cobalt2.utils").colors
+local Group = require("cobalt2.utils").Group
+local styles = require("cobalt2.utils").styles
+
+---------------------------------------------------------------------------------
+--                                 fidget.nvim                                 --
+---------------------------------------------------------------------------------
+Group.new("FidgetTitle", colors.green, colors.cobalt_bg, styles.bold)
+Group.new("FidgetTask", colors.blue, colors.cobalt_bg, nil)
