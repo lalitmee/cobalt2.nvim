96e57f88373960937385058781508f7abe029d06
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-04 12:12:46 +0530

    add gitsigns current line blame color

--- lua/cobalt2/plugins/gitsigns.lua
+++ lua/cobalt2/plugins/gitsigns.lua
@@ -7,3 +7,4 @@ local Group = require("cobalt2.utils").Group
 Group.new("GitSignsAdd", colors.green, nil, nil)
 Group.new("GitSignsChange", colors.yellow, nil, nil)
 Group.new("GitSignsDelete", colors.red, nil, nil)
+Group.new("GitSignsCurrentLineBlame", colors.light_grey:dark(), nil, nil)
