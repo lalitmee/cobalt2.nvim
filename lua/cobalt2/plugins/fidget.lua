bfa07a82a90f71a09163c17df9083d08356ec15a
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-04 20:57:56 +0530

    fix some colors

--- lua/cobalt2/plugins/fidget.lua
+++ lua/cobalt2/plugins/fidget.lua
@@ -5,5 +5,5 @@ local styles = require("cobalt2.utils").styles
 ---------------------------------------------------------------------------------
 --                                 fidget.nvim                                 --
 ---------------------------------------------------------------------------------
-Group.new("FidgetTitle", colors.green, colors.cobalt_bg, styles.bold)
+Group.new("FidgetTitle", colors.yellow, colors.cobalt_bg, styles.bold)
 Group.new("FidgetTask", colors.blue, colors.cobalt_bg, nil)
