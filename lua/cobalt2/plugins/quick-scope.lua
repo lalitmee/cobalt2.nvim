f57f624d358583387a10c5cbe75809e78a048ba5
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-07 19:50:56 +0530

    improve quick-scope colors

--- lua/cobalt2/plugins/quick-scope.lua
+++ lua/cobalt2/plugins/quick-scope.lua
@@ -1,5 +1,8 @@
+local Group = require("cobalt2/utils").Group
+local colors = require("cobalt2/utils").colors
+
 ---------------------------------------------------------------------------------
 --                                 quick-scope                                 --
 ---------------------------------------------------------------------------------
--- Group.new('QuickScopePrimary', colors.pink, nil, nil)
--- Group.new('QuickScopeSecondary', colors.red, nil, nil)
+Group.new("QuickScopePrimary", colors.black, colors.blue, nil)
+Group.new("QuickScopeSecondary", colors.red, colors.black, nil)
