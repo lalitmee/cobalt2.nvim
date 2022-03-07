555a73492d1290ecfa34c7c095bbbf96107a9583
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-07 14:56:31 +0530

    improved bufferline colors

--- lua/cobalt2/plugins/bufferline.lua
+++ lua/cobalt2/plugins/bufferline.lua
@@ -5,12 +5,12 @@ local Group = require("cobalt2.utils").Group
 ---------------------------------------------------------------------------------
 --                                 bufferline                                  --
 ---------------------------------------------------------------------------------
-Group.new("BufferLineFill", colors.light_grey, nil, nil)
+Group.new("BufferLineFill", colors.light_grey, colors.cursor_line, nil)
 Group.new("BufferLineBackground", colors.dark_blue, nil, styles.italic)
 Group.new("BufferLineBufferVisible", colors.dark_blue, nil, nil)
 Group.new("BufferLineBufferSelected", colors.yellow, nil, nil)
 Group.new("BufferLineTab", colors.white, nil, nil)
-Group.new("BufferLineTabSelected", colors.yellow, colors.dirty_blue, styles.bold)
+Group.new("BufferLineTabSelected", colors.yellow, colors.cursor_hover, styles.bold)
 Group.new("BufferLineTabClose", colors.red, nil, nil)
 Group.new("BufferLineIndicatorSelected", colors.dark_pink, nil, nil)
 -- separator
