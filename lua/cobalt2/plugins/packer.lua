95f469f8dde82318025bfcd7044609c0ca0ea7a8
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-16 11:57:30 +0530

    fix(pakcer): make packer text non italic

--- lua/cobalt2/plugins/packer.lua
+++ lua/cobalt2/plugins/packer.lua
@@ -1,11 +1,10 @@
 local colors = require("cobalt2.utils").colors
-local styles = require("cobalt2.utils").styles
 local Group = require("cobalt2.utils").Group
 
 ---------------------------------------------------------------------------------
 --                                 packer.nvim                                 --
 ---------------------------------------------------------------------------------
-Group.new("packerStatusSuccess", colors.yellow, nil, styles.italic)
-Group.new("packerWorking", colors.light_grey, nil, styles.italic)
-Group.new("packerStatusFail", colors.red:light(), nil, styles.italic)
-Group.new("packerFail", colors.red:light(), nil, styles.italic)
+Group.new("packerStatusSuccess", colors.yellow, nil, nil)
+Group.new("packerWorking", colors.light_grey, nil, nil)
+Group.new("packerStatusFail", colors.red:light(), nil, nil)
+Group.new("packerFail", colors.red:light(), nil, nil)
