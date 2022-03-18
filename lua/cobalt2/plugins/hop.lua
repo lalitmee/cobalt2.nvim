51a6e6ab0d3ca125188dbc4617329ed2cccc6fb2
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-18 16:37:46 +0530

    fix(hop): fix hop background color

--- lua/cobalt2/plugins/hop.lua
+++ lua/cobalt2/plugins/hop.lua
@@ -7,4 +7,4 @@ local Group = require("cobalt2.utils").Group
 Group.new("HopNextKey", colors.red, nil, nil)
 Group.new("HopNextKey1", colors.pink, nil, nil)
 Group.new("HopNextKey2", colors.blue, nil, nil)
-Group.new("HopUnmatched", colors.dark_grey, nil, nil)
+Group.new("HopUnmatched", colors.light_grey, nil, nil)
