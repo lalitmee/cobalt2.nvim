c7f265a728d5d9fd612943a143b667fc65383ec4
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-04-08 11:02:13 +0530

    feat(yanky): add `yanky.nvim` colors

--- lua/cobalt2/plugins/lualine.lua
+++ lua/cobalt2/plugins/lualine.lua
@@ -7,3 +7,5 @@ local Group = require("cobalt2.utils").Group
 Group.new("LualineSessionName", colors.darkest_blue, colors.blue, nil)
 Group.new("LualineLspClientName", colors.yellow, colors.cursor_hover, nil)
 Group.new("LualineFileName", colors.yellow, colors.cursor_line, nil)
+Group.new("LualineFileType", colors.dark_pink, colors.cursor_line, nil)
+Group.new("LualineFileSize", colors.light_blue, colors.cursor_line, nil)
---
+++ lua/cobalt2/plugins/yanky.lua
@@ -0,0 +1,8 @@
+local colors = require("cobalt2.utils").colors
+local Group = require("cobalt2.utils").Group
+
+--------------------------------------------------------------------------------
+-- yanky colors
+--------------------------------------------------------------------------------
+Group.new("YankyPut", colors.black, colors.blue, nil)
+Group.new("YankyYanked", colors.black, colors.yellow, nil)
