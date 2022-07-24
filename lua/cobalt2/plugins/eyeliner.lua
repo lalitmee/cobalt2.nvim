836e824614bac3b3171dadb3aa1424f5d06f5f3f
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-07-24 14:55:41 +0530

    fix(eyeliner): change colors

--- lua/cobalt2/plugins/eyeliner.lua
+++ lua/cobalt2/plugins/eyeliner.lua
@@ -4,5 +4,5 @@ local colors = require("cobalt2/utils").colors
 ---------------------------------------------------------------------------------
 --                                 quick-scope                                 --
 ---------------------------------------------------------------------------------
-Group.new("EyelinerPrimary", colors.black, colors.blue, nil)
+Group.new("EyelinerPrimary", colors.pink, colors.black, nil)
 Group.new("EyelinerSecondary", colors.red, colors.black, nil)
--- lua/cobalt2/plugins/quick-scope.lua
+++ lua/cobalt2/plugins/quick-scope.lua
@@ -4,5 +4,5 @@ local colors = require("cobalt2/utils").colors
 ---------------------------------------------------------------------------------
 --                                 quick-scope                                 --
 ---------------------------------------------------------------------------------
-Group.new("QuickScopePrimary", colors.black, colors.blue, nil)
+Group.new("QuickScopePrimary", colors.pink, colors.black, nil)
 Group.new("QuickScopeSecondary", colors.red, colors.black, nil)
