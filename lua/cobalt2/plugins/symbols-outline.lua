f16cc6578648ccb205edc00e2929b525dfbaa166
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-08-16 12:36:39 +0530

    feat(symbols-outline): add `highlights`

---
+++ lua/cobalt2/plugins/symbols-outline.lua
@@ -0,0 +1,8 @@
+local Group = require("cobalt2/utils").Group
+local colors = require("cobalt2/utils").colors
+
+---------------------------------------------------------------------------------
+--                                 quick-scope                                 --
+---------------------------------------------------------------------------------
+Group.new("FocusedSymbol", colors.yellow, colors.darker_blue, nil)
+Group.new("SymbolsOutlineConnector", colors.yellow, colors.darker_blue, nil)
