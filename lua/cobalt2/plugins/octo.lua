d72b7eb7138b3995881eb4ecf20544e295b6accd
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-09-24 16:49:41 +0530

    feat(octo.nvim): add `octo.nvim` colors

---
+++ lua/cobalt2/plugins/octo.lua
@@ -0,0 +1,13 @@
+local colors = require("cobalt2.utils").colors
+local Group = require("cobalt2.utils").Group
+
+------------------------------------------------------------------------------------------------------------------------
+--  octo.nvim
+------------------------------------------------------------------------------------------------------------------------
+Group.new("OctoEditable", colors.white, colors.darker_blue, nil)
+Group.new("OctoRedFloat", colors.red, nil, nil)
+Group.new("OctoBlueFloat", colors.blue, nil, nil)
+Group.new("OctoGreyFloat", colors.grey, nil, nil)
+Group.new("OctoGreenFloat", colors.green, nil, nil)
+Group.new("OctoPurpleFloat", colors.purple, nil, nil)
+Group.new("OctoYellowFloat", colors.yellow, nil, nil)
