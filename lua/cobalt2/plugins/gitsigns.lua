732f76f1286bb3f7733822c43dfe82d2a887fb0d
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-19 22:55:25 +0530

    refactor(git): change git colors

--- lua/cobalt2/plugins/gitsigns.lua
+++ lua/cobalt2/plugins/gitsigns.lua
@@ -4,4 +4,7 @@ local Group = require("cobalt2.utils").Group
 Group.new("GitSignsAdd", colors.green, nil, nil)
 Group.new("GitSignsChange", colors.yellow, nil, nil)
 Group.new("GitSignsCurrentLineBlame", colors.light_grey:dark(), nil, nil)
-Group.new("GitSignsDelete", colors.red, nil, nil)
+Group.new("GitSignsDelete", colors.dark_red, nil, nil)
+Group.new("GitSignsDeletePreview", colors.white, colors.dark_red, nil)
+Group.new("GitSignsDeleteVirtLn", colors.white, colors.dark_red, nil)
+Group.new("GitSignsAddPreview", colors.white, colors.darkest_green, nil)
