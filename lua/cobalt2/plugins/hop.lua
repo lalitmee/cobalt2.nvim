439a50bda7e1e19227b247ecc041474e73f33c20
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-12-31 21:38:50 +0530

    feat(hop): update `hop.nvim` colors

--- lua/cobalt2/plugins/hop.lua
+++ lua/cobalt2/plugins/hop.lua
@@ -1,7 +1,7 @@
 local colors = require("cobalt2.utils").colors
 local Group = require("cobalt2.utils").Group
 
-Group.new("HopNextKey", colors.yellow, colors.black, nil)
-Group.new("HopNextKey1", colors.green, colors.black, nil)
-Group.new("HopNextKey2", colors.red, colors.black, nil)
-Group.new("HopUnmatched", nil, nil, nil)
+Group.new("HopNextKey", colors.dark_orange, colors.black, nil)
+Group.new("HopNextKey1", colors.light_green, colors.black, nil)
+Group.new("HopNextKey2", colors.dark_pink, colors.black, nil)
+Group.new("HopUnmatched", colors.light_grey, nil, nil)
