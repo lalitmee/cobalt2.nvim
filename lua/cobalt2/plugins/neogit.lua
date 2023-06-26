74362418b19d891dadc986126a3adea1458d3372
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-06-26 12:38:22 +0530

    fix(neogit): context highlight color

--- lua/cobalt2/plugins/neogit.lua
+++ lua/cobalt2/plugins/neogit.lua
@@ -5,12 +5,7 @@ Group.new("NeogitBranch", colors.green, nil, nil)
 Group.new("NeogitDiffAdd", colors.green, nil, nil)
 Group.new("NeogitDiffAddHighlight", colors.green, nil, nil)
 Group.new("NeogitDiffAddRegion", colors.green, nil, nil)
-Group.new(
-    "NeogitDiffContextHighlight",
-    colors.white,
-    colors.cobalt_bg:light(),
-    nil
-)
+Group.new("NeogitDiffContextHighlight", nil, colors.cobalt_bg:light(), nil)
 Group.new("NeogitDiffDelete", colors.red, nil, nil)
 Group.new("NeogitDiffDeleteHighlight", colors.red, nil, nil)
 Group.new("NeogitDiffDeleteRegion", colors.red, nil, nil)
