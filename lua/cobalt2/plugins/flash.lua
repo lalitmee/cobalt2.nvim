8adf2876a020037f4768153a01697e6f22ac9d64
Kevin Burns <kevin.burns@hpe.com>
2024-06-18 14:44:07 -0500

    Change Flash Label to blue

--- lua/cobalt2/plugins/flash.lua
+++ lua/cobalt2/plugins/flash.lua
@@ -5,4 +5,4 @@ local Group, colors, styles = utils.Group, utils.colors, utils.styles
 Group.new("FlashBackdrop", colors.light_grey, colors.cobalt_bg, styles.italic)
 Group.new("FlashMatch", colors.black, colors.yellow, nil)
 Group.new("FlashCurrent", colors.black, colors.yellow, nil)
-Group.new("FlashLabel", colors.white, colors.pink, nil)
+Group.new("FlashLabel", colors.white, colors.blue, nil)
