d3feb30aeabb59f1419121748e5a42b5b631601b
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-01-13 17:20:20 +0530

    feat(blink-cmp): support `blink.cmp`

---
+++ lua/cobalt2/plugins/blink-cmp.lua
@@ -0,0 +1,8 @@
+local colors = require("cobalt2.utils").colors
+local styles = require("cobalt2.utils").styles
+local Group = require("cobalt2.utils").Group
+
+Group.new("BlinkCmpMenu", nil, colors.cobalt_bg, nil)
+Group.new("BlinkCmpMenuBorder", colors.cobalt_bg, colors.white, styles.bold)
+Group.new("BlinkCmpMenuSelection", nil, colors.cursor_hover, styles.bold)
+Group.new("BlinkCmpScrollBarGutter", nil, colors.cursor_hover, styles.bold)
