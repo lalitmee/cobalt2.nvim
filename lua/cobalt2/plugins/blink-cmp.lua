473510a7965a80cb29d79e6c245c3d391eb76e6b
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-01-13 17:30:13 +0530

    fix(blink-cmp): update `blink-cmp` border

--- lua/cobalt2/plugins/blink-cmp.lua
+++ lua/cobalt2/plugins/blink-cmp.lua
@@ -3,6 +3,6 @@ local styles = require("cobalt2.utils").styles
 local Group = require("cobalt2.utils").Group
 
 Group.new("BlinkCmpMenu", nil, colors.cobalt_bg, nil)
-Group.new("BlinkCmpMenuBorder", colors.cobalt_bg, colors.white, styles.bold)
+Group.new("BlinkCmpMenuBorder", colors.blue, nil, nil)
 Group.new("BlinkCmpMenuSelection", nil, colors.cursor_hover, styles.bold)
 Group.new("BlinkCmpScrollBarGutter", nil, colors.cursor_hover, styles.bold)
