9cbce9889178e15bd90e801419277ce0f1b1545d
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2025-01-14 11:28:40 +0530

    feat(blink-cmp): add doc and signature help borders

--- lua/cobalt2/plugins/blink-cmp.lua
+++ lua/cobalt2/plugins/blink-cmp.lua
@@ -2,7 +2,17 @@ local colors = require("cobalt2.utils").colors
 local styles = require("cobalt2.utils").styles
 local Group = require("cobalt2.utils").Group
 
+-- menu
 Group.new("BlinkCmpMenu", nil, colors.cobalt_bg, nil)
 Group.new("BlinkCmpMenuBorder", colors.blue, nil, nil)
 Group.new("BlinkCmpMenuSelection", nil, colors.cursor_hover, styles.bold)
 Group.new("BlinkCmpScrollBarGutter", nil, colors.cursor_hover, styles.bold)
+
+-- documentation
+Group.new("BlinkCmpDocBorder", colors.blue, nil, nil)
+Group.new("BlinkCmpDocSeparator", colors.blue, nil, nil)
+Group.new("BlinkCmpDocCursorLine", colors.cursor_line, nil, nil)
+
+-- signature
+Group.new("BlinkCmpSignatureHelpBorder", colors.blue, nil, nil)
+Group.new("BlinkCmpSignatureHelpActiveParameter", nil, colors.cursor_hover, nil)
